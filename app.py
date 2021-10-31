653ce43ceaacc8dc55fd6a52f24652e19d7791ea
said <eger3612@gmail.com>
2021-10-31 12:27:13 +0100

    feat : login form front end

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():  # put application's code here
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():  # put application's code here
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
